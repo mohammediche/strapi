79e90ce13ce82efbc3eb3ee9e4d088d6f246ad12
mohammediche <mohammediche@gmail.com>
2025-09-04 07:22:56 +0200

    feat: add artciles blog for propale

--- config/server.js
+++ config/server.js
@@ -2,7 +2,8 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    // keys: env.array(process.env.NEXT_PUBLIC_STRAPI_URL),
+    keys: env.array('APP_KEYS', ['myKeyA', 'myKeyB']),
   },
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
